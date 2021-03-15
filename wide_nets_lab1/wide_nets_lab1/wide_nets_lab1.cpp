705248d7987c9a3c429ec528c50e3352f88a13b5
Eliz9347 <repkina-liz@mail.ru>
2021-03-15 10:05:32 +0300

    add functions for adding mistakes

--- wide_nets_lab1/wide_nets_lab1/wide_nets_lab1.cpp
+++ wide_nets_lab1/wide_nets_lab1/wide_nets_lab1.cpp
@@ -11,6 +11,11 @@
 #include <string>
 #include <bitset>
 
+#include <ctime>
+#include <cstdlib>
+
+#include <fstream>
+#include <cstring>
 
 // Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
 #pragma comment (lib, "Ws2_32.lib")
@@ -18,33 +23,31 @@
 #pragma comment (lib, "AdvApi32.lib")
 
 
-#define DEFAULT_BUFLEN 512
+#define DEFAULT_BUFLEN 5001
 #define DEFAULT_PORT "27015"
 
-//byte[] textBytes
-
-
+// Функция кодирования слова кодом Хемминга
 std::string wordencode(std::string binword) {
-  int control = 5;
-  int len = 15;
-  int codelen = 20;
-  int matr[5]{ 0 };
-  int array[20];
+  int control = 7;
+  int len = 71;
+  int codelen = 78;
+  int matr[7]{ 0 };
+  int array[78]{0};
 
   //std::string encodedString = "00100010001011100001";
   std::string encodedString = binword;
 
   for (int i = 0; i < codelen; i++) {
     array[i] = (int)encodedString[i] - 48;
-    std::cout << array[i];
+    //std::cout << array[i];
   }
-  std::cout << "\n";
+ // std::cout << "\n";
 
   // Расчёт контрольных битов
   for (int i = 0; i < codelen; i++) {
-    std::bitset<5> b(i + 1);
+    std::bitset<7> b(i + 1);                            // можно сделать лучше
     //std::cout << b.to_string() << "\n";
-    for (int j = 0; j < 5; j++) {
+    for (int j = 0; j < control; j++) {
       matr[j] += b[j] * (array[i]);
     }
   }
@@ -63,92 +66,200 @@ std::string wordencode(std::string binword) {
     res += std::to_string(array[i]);
   }
 
-  std::cout << "Закодировано" << std::endl;
-  std::cout << res << " длина " << res.size() << "\n";
+  //std::cout << "Закодировано" << std::endl;
+  //std::cout << res << " длина " << res.size() << "\n";
 
-  std::cout << "Добавление бита чётности" << std::endl;
+  //std::cout << "Добавление бита чётности" << std::endl;
   int evenbit(0); // бит чётности
   for (int i = 0; i < codelen; i++) {
     evenbit += array[i];
   }
   res += std::to_string(evenbit%2);
-  std::cout << res << " длина " << res.size() << "\n";
+  //std::cout << res << " длина " << res.size() << "\n";
 
   return res;
 }
 
-
-
+// Функция кодирования текста кодом Хемминга
 std::string textencode(std::string binword) {
-  int len = 15;
-  int control = 5;
+  int len = 71;
+  int control = 7;
 
-  std::cout << "Кодирование сообщения кодом Хемминга с длиной информационного слова 15" << std::endl;
+  std::cout << "Кодирование сообщения кодом Хемминга с длиной информационного слова 71" << std::endl;
   // Кодирование сообщения
   std::string binaryString = "";
   for (char& _char : binword) {
     binaryString += std::bitset<8>(_char).to_string();
   }
-  std::cout << binaryString << " длина "<< binaryString.size() << std::endl;
+  //std::cout << binaryString << " длина "<< binaryString.size() << std::endl;
+  std::cout << "Длина двоичного " << binaryString.size() << std::endl;
 
   // строка для закодированного сообщения
   std::string res = "";
 
   // Разбивка сообщения на слова длины 15
-  std::cout << "Разбивка сообщения\n";
+  //std::cout << "Разбивка сообщения\n";
   for (int k = 0; k < binaryString.size(); k += len) {
     std::string word = binaryString.substr(k, len);
     
-    std::cout << "\nИсходное информационное слово" << std::endl;
-    std::cout << word << " длина " << word.size() << std::endl;
+    //std::cout << "\nИсходное информационное слово" << std::endl;
+    //std::cout << word << " длина " << word.size() << std::endl;
 
     
     // Добавление нулей к последнему слову
     if (word.size() < len) {
-      std::cout << "Добавление нулей при необходимости" << std::endl;
+      //std::cout << "Добавление нулей при необходимости" << std::endl;
       int extr = len - word.size();
       //std::cout << extr << std::endl;
       for (int i = 0; i < extr; i++) {
         word += "0";
       }
-      std::cout << word << " длина " << word.size() << std::endl;
+      //std::cout << word << " длина " << word.size() << std::endl;
     }
     
     // Добавление контрольных битов
-    std::cout << "Добавление нулевых контрольных битов" << std::endl;
+    //std::cout << "Добавление нулевых контрольных битов" << std::endl;
     for (int i = 0; i < control; i++) {
       int p = pow(2, i) - 1;
       word.insert(p, std::string("0"));
     }
 
-    std::cout << word << " длина " << word.size() << std::endl;
-    std::cout << "Кодирование слова\n";
+    //std::cout << word << " длина " << word.size() << std::endl;
+    //std::cout << "Кодирование слова\n";
     res += wordencode(word);
-    std::cout << "Результат слова\n" << res << " длина " << res.size() << "\n";
+    //std::cout << "Результат слова\n" << res << " длина " << res.size() << "\n";
+  }
+  std::cout << "Закодировано\n";
+  //std::cout << "Длина двоичного закодированного " << res.size() << std::endl;
+
+  // Закодировано для сжатия
+  // Добавление недостающих нулей для преобразования в символы
+ /* int rest = res.size() % 8;
+  std::cout << "Остаток " << rest << std::endl;*/
+
+  //if (rest != 0) {
+  //  //std::cout << "Добавление нулей при необходимости" << std::endl;
+  //  int extr = 8-rest;
+  //  for (int i = 0; i < extr; i++) {
+  //    res += "0";
+  //  }
+  //  std::cout << "Длина двоичного удлинённого" << res.size() << std::endl;
+  //  std::cout << "Остаток " << res.size() % 8 << std::endl;
+  //}
+
+
+  //std::string text = "";
+  //for (int i = 0; i < res.size(); i += 8) {
+  //  std::string temp = res.substr(i, 8);
+  //  std::bitset<8> mybs(temp);
+  //  char ch = mybs.to_ullong();
+  //  text += ch;
+  //}
+
+  //std::cout << "Длина двоичного закодированного " << text.size() << std::endl;
+  return res;
+}
+
+// Функция добавления единичных ошибок в сообщение, возвращает количество ошибок
+int addmistake(std::string &message, int freq) {
+  
+  int n = message.size() / freq;  // максимальное количество ошибок в сообщении
+  int mistakes(0); // реальное количество добавленных ошибок в сообщение
+  float probability = float(rand()); // сброс первого
+
+  for (int i = 0; i < n; i++) {
+    probability = float(rand()) / float(RAND_MAX);
+    //std::cout << "Вероятность ошибки в слове " << probability << "\n";
+    // добавление ошибки
+    if (probability+0.2 > 0.5) {
+      int position = i*freq+(int)(rand() % (freq - 1)); // диапазон равен от 0 до freq (длина слова) включительно
+      //std::cout << "Позиция " << position << "\n";
+      //std::cout << "старый символ " << message[position] << "\n";
+      if (message[position] == '1') { // проверить работу
+        message.replace(position, 1, "0");
+      }
+      else {
+        message.replace(position, 1, "1");
+      }
+      //std::cout << "новый символ " << message[position] << "\n";
+      mistakes++;
+    }
   }
 
-  return res;
+  //std::cout << "Число ошибок " << mistakes << "\n";
+  //std::cout << "новое сообщение\n" << message<< "\n";
+  return mistakes;
+}
+
+// Функция добавления множественных ошибок (не более n на слово) в сообщение, возвращает количество ошибок
+int addnmistakes(std::string& message, int freq, int n) {
+  int allmistakes(0);
+  int tempmistakes(0);
+  for (int i = 0; i < n; i++) {
+    std::cout << "Добавление порции ошибок " << "\n";
+    tempmistakes = addmistake(message, freq);
+    allmistakes += tempmistakes;
+  }
+  return allmistakes;
 }
 
 
 int __cdecl main(int argc, char** argv)
 {
-  setlocale(LC_ALL, "Russian");
+  //setlocale(LC_ALL, "Russian");
+  setlocale(LC_ALL, "ru_RU.UTF8");
+  //SetConsoleCP(1251);// установка кодовой страницы win-cp 1251 в поток ввода
+  //SetConsoleOutputCP(1251); // установка кодовой страницы win-cp 1251 в поток вывода
+  //char string[20];
+  //std::cin >> string; // вводим строку, используя Кириллицу
+  //std::cout << "\nвывод: " << string << std::endl; // ввывод строки
   WSADATA wsaData;
   SOCKET ConnectSocket = INVALID_SOCKET;
   struct addrinfo* result = NULL,
     * ptr = NULL,
     hints;
-  //const char* sendbuf = "this is a test Интересно";
 
-  std::string s = "Long Text";
+  srand(time(NULL));
+
+  std::string strInput;
+  std::string message;
+  
+
+  // окрываем файл для чтения
+  std::ifstream in("f.txt");
+  if (in.is_open())
+  {
+    while (getline(in, strInput))
+    {
+      message += strInput;
+    }
+  }
+  in.close();
+
+  /*const char* arr = message.c_str();
+
+  std::cout << "\nСимвольный массив: \n";
+  std::cout << arr << std::endl;*/
+
+  std::cout << message << std::endl;
+  setlocale(LC_ALL, "Russian");
+  std::cout << "\nДлина сообщения: \n" << message.size() << std::endl;
+
   // Кодирование сообщения
   std::cout << "\nКодирование сообщения\n";
-  std::string coderes = textencode(s);
-  std::cout << "\nРезультат\n" << coderes << "\n";
+  std::cout << "Количество ошибок:\n" << 0 << "\n";
+  std::string coderes = textencode(message);
+  std::cout << "\nРезультат\n" << coderes.size() << "\n";
+  
+  std::string coderes_one_err = coderes;
+  std::cout << "\nДобавление ошибок\n";
+  int mistakes = addmistake(coderes_one_err, 79);
+  std::cout << "Количество единичных ошибок:\n" << mistakes << "\n";
+  //int mistakes = addnmistakes(coderes, 79, 2);
+  //std::cout << "\nРезультат\n" << coderes_err << "\n";
+  
   std::cout << "\n\n";
-
-  const char* sendbuf = coderes.c_str();
+   
 
   char recvbuf[DEFAULT_BUFLEN];
   int iResult;
@@ -220,16 +331,73 @@ int __cdecl main(int argc, char** argv)
     return 1;
   }
 
-  // Send an initial buffer
-  iResult = send(ConnectSocket, sendbuf, (int)strlen(sendbuf), 0);
-  if (iResult == SOCKET_ERROR) {
-    printf("send failed with error: %d\n", WSAGetLastError());
-    closesocket(ConnectSocket);
-    WSACleanup();
-    return 1;
+
+  const char* sendbuf;
+  
+  /*sendbuf = ("len"+std::to_string(3*coderes.length())).c_str();
+  iResult = send(ConnectSocket, sendbuf, (int)strlen(sendbuf), 0);*/
+  
+
+  int k(0);
+  while (k < coderes_one_err.length()) {
+    std::string ter = coderes_one_err.substr(k, 5000);
+    sendbuf = ter.c_str();
+    std::cout << "\n Часть сообщения\n" << std::endl;
+    //std::cout << sendbuf << std::endl;
+    k += 5000;
+
+    // Send an initial buffer
+    iResult = send(ConnectSocket, sendbuf, (int)strlen(sendbuf), 0);
+    if (iResult == SOCKET_ERROR) {
+      printf("send failed with error: %d\n", WSAGetLastError());
+      closesocket(ConnectSocket);
+      WSACleanup();
+      return 1;
+    }
+    printf("Bytes Sent: %ld\n", iResult);
+
   }
 
-  printf("Bytes Sent: %ld\n", iResult);
+  //std::cout << "\n Отправка второго сообщения\n" << std::endl;
+  //k = 0;
+  //while (k < coderes_one_err.length()) {
+  //  std::string ter = coderes_one_err.substr(k, 5000);
+  //  sendbuf = ter.c_str();
+  //  std::cout << "\n Часть сообщения\n" << std::endl;
+  //  //std::cout << sendbuf << std::endl;
+  //  k += 5000;
+
+  //  iResult = send(ConnectSocket, sendbuf, (int)strlen(sendbuf), 0);
+  //  if (iResult == SOCKET_ERROR) {
+  //    printf("send failed with error: %d\n", WSAGetLastError());
+  //    closesocket(ConnectSocket);
+  //    WSACleanup();
+  //    return 1;
+  //  }
+  //  printf("Bytes Sent: %ld\n", iResult);
+
+  //}
+
+  //std::cout << "\n Отправка третьего сообщения\n" << std::endl;
+  //k = 0;
+  //while (k < coderes_many_err.length()) {
+  //  std::string ter = coderes_many_err.substr(k, 5000);
+  //  sendbuf = ter.c_str();
+  //  std::cout << "\n Часть сообщения\n" << std::endl;
+  //  //std::cout << sendbuf << std::endl;
+  //  k += 5000;
+
+  //  iResult = send(ConnectSocket, sendbuf, (int)strlen(sendbuf), 0);
+  //  if (iResult == SOCKET_ERROR) {
+  //    printf("send failed with error: %d\n", WSAGetLastError());
+  //    closesocket(ConnectSocket);
+  //    WSACleanup();
+  //    return 1;
+  //  }
+  //  printf("Bytes Sent: %ld\n", iResult);
+
+  //}
+
 
   // shutdown the connection since no more data will be sent
   iResult = shutdown(ConnectSocket, SD_SEND);
@@ -240,7 +408,8 @@ int __cdecl main(int argc, char** argv)
     return 1;
   }
 
-  int re;// проба
+  std::string decoded;
+  int re(0);// проба
   // Receive until the peer closes the connection
   do {
 
@@ -248,27 +417,38 @@ int __cdecl main(int argc, char** argv)
     if (re > 0) {
       printf("Bytes received: %d\n", re);
       recvbuf[re] = '\0';
-      std::cout << "From the node: " << recvbuf << std::endl;
       
-      std::string decoded = recvbuf;
+      
+      if (re > 50) {
+        
+        setlocale(LC_ALL, "ru_RU.UTF8");
+        decoded = recvbuf;
+        std::cout << "Length: \n" << decoded.length() << "\n";
+        //std::cout << decoded << "\n";
+        //Обработка статистической информации об ошибках
+        int underline = decoded.find('_');
+        int zero = stoi(decoded.substr(0, underline));
+        decoded.erase(0, underline + 1);
 
-      // Обработка статистической информации об ошибках
-      int underline = decoded.find('_');
-      int zero = stoi(decoded.substr(0, underline));
-      decoded.erase(0, underline+1);
+        underline = decoded.find('_');
+        int one = stoi(decoded.substr(0, underline));
+        decoded.erase(0, underline + 1);
 
-      underline = decoded.find('_');
-      int one = stoi(decoded.substr(0, underline));
-      decoded.erase(0, underline+1);
+        underline = decoded.find('_');
+        int many = stoi(decoded.substr(0, underline));
+        decoded.erase(0, underline + 1);
+        std::cout << "Текст:\n" << decoded << "\n";
 
-      underline = decoded.find('_');
-      int many = stoi(decoded.substr(0, underline));
-      decoded.erase(0, underline+1);
-      std::cout << "Количество правильно доставленных слов: "<< zero << "\n";
-      std::cout << "Количество исправленных ошибок: " << one << "\n";
-      std::cout << "Количество множественных ошибок: " << many << "\n";
-      std::cout << "Текст:\n" << decoded << "\n";
-     
+        setlocale(LC_ALL, "Russian");
+        std::cout << "Количество правильно доставленных слов: " << zero << "\n";
+        std::cout << "Количество исправленных ошибок: " << one << "\n";
+        std::cout << "Количество множественных ошибок: " << many << "\n";
+       
+      }
+      else {
+        std::cout << "From the node: " << recvbuf << std::endl;
+      }
+      
     }
       
     else if (iResult == 0)
@@ -279,10 +459,12 @@ int __cdecl main(int argc, char** argv)
   } while (re > 0);
 
 
+ 
+
   //system("PAUSE");
 
   // cleanup
-  closesocket(ConnectSocket);
+   closesocket(ConnectSocket);
   WSACleanup();
 
 
--- wide_nets_lab1/wide_nets_lab1_server/wide_nets_lab1_server.cpp
+++ wide_nets_lab1/wide_nets_lab1_server/wide_nets_lab1_server.cpp
@@ -17,37 +17,38 @@
 #pragma comment (lib, "Ws2_32.lib")
 // #pragma comment (lib, "Mswsock.lib")
 
-#define DEFAULT_BUFLEN 512
+//#define DEFAULT_BUFLEN 512
+#define DEFAULT_BUFLEN 5001
 #define DEFAULT_PORT "27015"
 
-
+// Функция декодирования слова, закодированного кодом Хемминга
 int worddecode(std::string binword) {
-  int control = 5;
-  int len = 15;
-  int codelen = 20;
-  int array[20];
-  int mistake[5]{ 0 };
+  int control = 7;
+  int len = 71;
+  int codelen = 78;
+  int array[78]{ 0 };
+  int mistake[7]{ 0 };
 
   for (int i = 0; i < codelen; i++) {
-    array[i] = (int)binword[i]-48;        // Ошибка тут!!!
-    std::cout << array[i];
+    array[i] = (int)binword[i]-48;
+    //std::cout << array[i];
   }
 
   for (int i = 0; i < codelen; i++) {
-    std::bitset<5> b(i + 1);
+    std::bitset<7> b(i + 1);
     for (int j = 0; j < control; j++) {
       mistake[j] += b[j] * (array[i]);
     }
   }
 
   int error(0);
-  std::cout << "\nматрица синдромов\n";
+  //std::cout << "\nматрица синдромов\n";
   for (int i = 0; i < control; i++) {
     mistake[i] %= 2;
-    std::cout << mistake[i];
+    //std::cout << mistake[i];
     error += mistake[i] * pow(2, i); 
   }
-  std::cout << "\nОшибка " << error << "\n";
+  //std::cout << "\nОшибка " << error << "\n";
 
   //Проверка на чётность
   int evenbit((int)binword[codelen] - 48); // бит чётности
@@ -58,9 +59,11 @@ int worddecode(std::string binword) {
 
   int res;
   if (evenbit) {
-    std::cout << "Единичная ошибка в" << error << "\n";
-    res = 1;
-                                                        // Исправление ошибки нужно дописать
+    //std::cout << "Единичная ошибка в " << error << "\n";
+    res = error+2;
+    //std::cout << array[error] << std::endl;
+    //binword.replace(error, 1, std::to_string(not array[error]));
+    //std::cout << "Исправленное слово\n" << binword << "\n";
   }
   else {
     if (error) {
@@ -68,25 +71,32 @@ int worddecode(std::string binword) {
       res = 2;
     }
     else {
-      std::cout << "Нет ошибок " << "\n";
+      //std::cout << "Нет ошибок " << "\n";
       res = 0;
     }
   } 
-  std::cout << res  << "\n";
+  //std::cout << res  << "\n";
 
   return res;
 }
 
-
+// Функция декодирования текста, закодированного кодом Хемминга
 std::string textdecode(std::string bintext) {
-  int codelen = 20; // длина кодового слова без бита чётности
-  int control = 5;
+  int codelen = 78; // длина кодового слова без бита чётности
+  int control = 7;
 
   std::cout << "Декодирование сообщения" << std::endl;
+  //std::cout << encodedtext << std::endl;
+  //std::cout << " длина " << encodedtext.size() << std::endl;
+  //std::string bintext = "";
+  //for (char& _char : encodedtext) {
+  //  bintext += std::bitset<8>(_char).to_string();
+  //}
+  //std::cout << bintext << std::endl;
   //std::string binaryString = "100100011000111011110110110011100101001";
-  std::cout << bintext << " длина " << bintext.size() << std::endl;
+  std::cout << " длина " << bintext.size() << std::endl;
 
-  int res(0); //убрать лишний символ
+  int res(0);
 
   //для сбора статистики по количеству ошибок
   int zero(0);
@@ -95,44 +105,51 @@ std::string textdecode(std::string bintext) {
   // для преобразования сообщения в текст
   std::string symbols;
 
-  // Разбивка на кодовые слова длиной 21
+  // Разбивка на кодовые слова длиной 79
   std::cout << "Разбивка на кодовые слова\n";
   for (int k = 0; k < bintext.size(); k += (codelen+1)) {
     std::string codeword = bintext.substr(k, (codelen+1));
 
-    std::cout << "\nКодовое слово\n";
-    std::cout << codeword << " длина " << codeword.size() << std::endl;
-    std::cout << "Декодирование слова\n";
+    //std::cout << "\nКодовое слово\n";
+    //std::cout << codeword << " длина " << codeword.size() << std::endl;
+    //std::cout << "Декодирование слова\n";
     res = worddecode(codeword);
-    std::cout << "Результат слова\n" << res << "\n";
+    //std::cout << "Результат слова\n" << res << "\n";
+
 
     // обработка статистики по ошибкам
     if (res == 0) {
       zero++;
     }
-    else if (res == 1){
+    else if (res > 2){
       one++;
+      //Исправление единичной ошибки
+      int nsymbol = not (bool)codeword[res-3];
+      //std::cout << "старый символ " << codeword[res - 3] << "\n";
+      //std::cout << "новый символ " << nsymbol << "\n";
+      codeword.replace(res-3, 1, std::to_string(nsymbol));
+      //std::cout << "Исправленное слово\n" << codeword << "\n";
     }
     else {
       many++;
     }
 
     // Удаление контрольных битов
-      std::cout << "Удаление бита чётности и контрольных битов\n";
+    //std::cout << "Удаление бита чётности и контрольных битов\n";
     codeword.erase(codelen, 1);
     for (int i = control - 1; i >= 0; i--) {
       int p = pow(2, i) - 1;
       codeword.erase(p, 1);
     }
-    std::cout << codeword << " длина " << codeword.size() << "\n";
+    //std::cout << codeword << " длина " << codeword.size() << "\n";
 
-    std::cout << "Строка символов\n";
+    //std::cout << "Строка символов\n";
     symbols += codeword;
-    std::cout << codeword << " длина " << symbols.size() << "\n";
+    //std::cout << symbols << " длина " << symbols.size() << "\n";
 
   }
-
-  std::cout << "Teкст\n";
+  
+  //std::cout << "Teкст\n";
   std::string text = "";
   for (int i = 0; i < symbols.size(); i += 8) {
     std::string temp = symbols.substr(i, 8);
@@ -154,6 +171,7 @@ std::string textdecode(std::string bintext) {
 int __cdecl main(void)
 {
   setlocale(LC_ALL, "Russian");
+
   WSADATA wsaData;
   int iResult;
 
@@ -166,6 +184,8 @@ int __cdecl main(void)
   int iSendResult;
   char recvbuf[DEFAULT_BUFLEN];
   int recvbuflen = DEFAULT_BUFLEN;
+
+  std::cout << "Teкст\n";
 
   // Initialize Winsock
   iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
@@ -229,33 +249,56 @@ int __cdecl main(void)
   // No longer need server socket
   closesocket(ListenSocket);
   const char* sendbuf;
-
+  std::string mess;
+  //iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
+  /*std::string vol = recvbuf;
+  int volume = stoi(vol);*/
   // Receive until the peer shuts down the connection
   do {
 
     iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
-
+    //std::cout << "Vol: " << volume << std::endl; // проба
+   /* volume -= iResult;*/
     recvbuf[iResult] = '\0';
-    std::cout << "From the node: " << recvbuf << std::endl; // проба
+    //std::cout << "From the node: " << recvbuf << std::endl; // проба
     if (iResult > 0) {
       printf("Bytes received: %d\n", iResult);
-    
-    //Декодирование сообщения 
-      std::string encoded = recvbuf;
-      std::string res_flag = textdecode(encoded);
-      std::cout << "\nРезультат декодирования слова\n" << res_flag << "\n";
 
-      sendbuf = res_flag.c_str();
-      //sendbuf = recvbuf;
 
-      // Echo the buffer back to the sender
-      iSendResult = send(ClientSocket, sendbuf, iResult, 0);
-      if (iSendResult == SOCKET_ERROR) {
-        printf("send failed with error: %d\n", WSAGetLastError());
-        closesocket(ClientSocket);
-        WSACleanup();
-        return 1;
+      mess += recvbuf;
+      std::cout << mess.size() << " уже\n";
+
+      if (mess.size() > 35000) {
+        setlocale(LC_ALL, "ru_RU.UTF8");
+        std::string res_flag = textdecode(mess);
+        mess = "";
+       
+        sendbuf = res_flag.c_str();
+        iSendResult = send(ClientSocket, sendbuf, (int)strlen(sendbuf), 0);
+
+        if (iSendResult == SOCKET_ERROR) {
+          printf("send failed with error: %d\n", WSAGetLastError());
+          closesocket(ClientSocket);
+          WSACleanup();
+          return 1;
+        }
+        std::cout << "Test: " << sendbuf << std::endl; // проба
+        //std::cout << "Test: " << std::endl; // проба
+
       }
+      else {
+        sendbuf = "ok";
+        std::cout << mess.size() << std::endl;
+        iSendResult = send(ClientSocket, sendbuf, (int)strlen(sendbuf), 0);
+        if (iSendResult == SOCKET_ERROR) {
+          printf("send failed with error: %d\n", WSAGetLastError());
+          closesocket(ClientSocket);
+          WSACleanup();
+          return 1;
+        }
+      }   
+      
+      
       printf("Bytes sent: %d\n", iSendResult);
     }
     else if (iResult == 0)
@@ -266,9 +309,11 @@ int __cdecl main(void)
       WSACleanup();
       return 1;
     }
-    system("PAUSE"); // Добавлено, чтобы не закрывался
-  } while (iResult > 0);
 
+  } while (iResult > 0);
+    //(volume > 0);
+
+  Sleep(1000);
   // shutdown the connection since we're done
   iResult = shutdown(ClientSocket, SD_SEND);
   if (iResult == SOCKET_ERROR) {
@@ -277,7 +322,7 @@ int __cdecl main(void)
     WSACleanup();
     return 1;
   }
-
+  system("PAUSE"); // Добавлено, чтобы не закрывался
   
 
   // cleanup
